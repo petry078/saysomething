07bef93efd36404ebcfeaee9cc239aa143111a1e
petry <petry@pop-os.localdomain>
2023-06-24 00:37:39 -0300

    it works...

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 4567
+let something = "something"
+
+//json parse
+app.use(express.json());
+
+//get
+app.get("/", (req, res) => {
+    res.json(something)
+})
+
+//post
+app.post("/", (req, res) => {
+    const somethingNew = req.body.string
+    something = somethingNew
+    return res.json({message: 'said.'})
+    })
+
+
+//npm start log
+app.listen(port, () => {
+    console.log(`port ${port}`)
+})
+
+
